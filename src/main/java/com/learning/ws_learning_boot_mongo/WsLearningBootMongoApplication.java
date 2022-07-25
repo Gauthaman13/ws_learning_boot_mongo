f94a6fffc7a95b1cd0a78fd868bb0dc957e3229b
Gauthaman Deenadayalan <gauthamandeenadayalan@gmail.com>
2022-07-25 09:26:13 -0500

    Add files via upload

---
+++ src/main/java/com/learning/ws_learning_boot_mongo/WsLearningBootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.learning.ws_learning_boot_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsLearningBootMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsLearningBootMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/ws_learning_boot_mongo/WsLearningBootMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ws_learning_boot_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsLearningBootMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
